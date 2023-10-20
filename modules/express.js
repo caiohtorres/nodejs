ca8fb690cc3add43dc914265d90c02f4088f5040
Caio <torrescaio12@gmail.com>
2023-10-20 10:03:17 -0300

    add, remove, update, find

--- modules/express.js
+++ modules/express.js
@@ -8,25 +8,49 @@ app.get("/home", (req, res) => {
   res.status(200).send("<h1> home pag express </h1>");
 });
 
-app.get("/users", (req, res) => {
-  const users = [
-    {
-      name: "caio",
-      email: "caio@ufla",
-    },
-    {
-      name: "rafael",
-      email: "rafael@ufla",
-    },
-  ];
+app.get("/users", async (req, res) => {
+  try {
+    const user = await UserModel.find({});
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
 
-  res.status(200).json(users);
+app.get("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await UserModel.findById(id);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
 app.post("./users", async (req, res) => {
   try {
     const user = await UserModel.create(req.body);
     res.status(201).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.patch("./users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await UserModel.findByIdAndUpdate(id, req.body, { new: true });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await UserModel.findByIdAndRemove(id);
+    res.status(200).json(user);
   } catch (error) {
     res.status(500).send(error.message);
   }
